1d3e42b21bcca0d8a0c32dba038a98f6f161cd66
shane gregory <shane.gregory@outlook.com>
2017-05-30 21:20:53 -0500

    timer console application

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hey ninja's");
+var time =0;
+
+var timer  = setInterval(function(){
+    time+= 2;
+    console.log(time +' seconds have passed');
+
+    if(time> 5){
+        clearInterval(timer);
+    }
+},2000);
